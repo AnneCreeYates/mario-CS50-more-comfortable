421f5a25e5da5bcb67173e814e1d9e0f8d1830fd
AnneC.Yates <annecreeyates@gmail.com>
2021-09-11 21:02:37 +0100

    Update mario.c
    adds the code that is to be submitted to CS50 more comfortable

--- mario.c
+++ mario.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int main(void)
+{
+    int height = 0;
+
+    //Asks the user to provide a number between 1 nad 8 inclusive
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height > 8 || height < 1);
+
+    for (int i = 0; i <= height - 1; i++)
+    {
+        for (int j = 0; j < height - i - 1; j++)
+        {
+            printf(" ");
+        }
+
+
+
+        for (int k = 0; k <= i; k++)
+        {
+            {
+                printf("#");
+            }
+
+        }
+
+        printf("  ");
+
+        for (int k = 0; k <= i; k++)
+        {
+            {
+                printf("#");
+            }
+
+        }
+
+
+        printf("\n");
+
+    }
+
+}
